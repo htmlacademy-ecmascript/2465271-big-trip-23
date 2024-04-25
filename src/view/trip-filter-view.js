33236caaac00b37027b08a8c39cd06f4f5a197ce
Eyeballeat <eyeballeat@gmail.com>
2024-04-25 23:25:24 +0200

    Доработаны файлы сортировки и фильтрации

--- src/view/trip-filter-view.js
+++ src/view/trip-filter-view.js
@@ -1,25 +1,18 @@
 import { createElement } from '../render';
 
+const TRIP_FILTERS_NAMES = ['everything', 'future', 'present', 'past'];
+
+const createFilterElement = (name) =>
+  `<div class="trip-filters__filter">
+      <input id="filter-${name}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${name}">
+      <label class="trip-filters__filter-label" for="filter-${name}">${name}</label>
+  </div>`;
+
 const createFilterTemplate = () =>
   `<form class="trip-filters" action="#" method="get">
-    <div class="trip-filters__filter">
-      <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything">
-      <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-    </div>
-    <div class="trip-filters__filter">
-      <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
-      <label class="trip-filters__filter-label" for="filter-future">Future</label>
-    </div>
-    <div class="trip-filters__filter">
-      <input id="filter-present" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="present">
-      <label class="trip-filters__filter-label" for="filter-present">Present</label>
-    </div>
-    <div class="trip-filters__filter">
-      <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past" checked>
-      <label class="trip-filters__filter-label" for="filter-past">Past</label>
-    </div>
-    <button class="visually-hidden" type="submit">Accept filter</button>
+  ${TRIP_FILTERS_NAMES.map((name) => createFilterElement(name)).join('')}
   </form>`;
+
 
 export default class TripFilterView {
   getTemplate() {
