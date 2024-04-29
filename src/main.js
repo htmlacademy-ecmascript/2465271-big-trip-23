b0999c357f42bea4e471e9a36ea549c575e30643
Eyeballeat <eyeballeat@gmail.com>
2024-04-29 20:02:50 +0200

    Шаблонизируй это

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import HeaderPagePresenter from './presenter/header-presenter';
 import MainPagePresenter from './presenter/main-presenter';
+import EventModel from './model/event-model';
 
 const pageHeaderFiltersElement = document.querySelector('.trip-controls__filters');
 const pageMainTripEventElement = document.querySelector('.trip-events');
-const mainPagePresenter = new MainPagePresenter({boardContainer: pageMainTripEventElement});
+const eventModel = new EventModel();
+const mainPagePresenter = new MainPagePresenter({
+  boardContainer: pageMainTripEventElement,
+  eventModel,
+});
 const headerPagePresenter = new HeaderPagePresenter({boardContainer: pageHeaderFiltersElement});
 
 headerPagePresenter.init();
--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -9,18 +9,37 @@ export default class MainPagePresenter {
   eventSortComponent = new TripSortView();
   eventListComponent = new TripListView();
 
-  constructor({boardContainer}) {
+  constructor({boardContainer, eventModel}) {
     this.boardContainer = boardContainer;
+    this.eventModel = eventModel;
   }
 
   init() {
+    this.eventOffers = [...this.eventModel.getOffers()];
+    this.eventDestinations = [...this.eventModel.getDestinations()];
+
     render(this.eventSortComponent, this.boardContainer);
     render(this.eventListComponent, this.boardContainer);
-    render(new TripEditView(), this.eventListComponent.getElement());
-    render(new TripCreateView(), this.eventListComponent.getElement());
+    render(new TripEditView
+    (
+      {offerData: this.eventOffers},
+      {destinationData: this.eventDestinations},
+    )
+    , this.eventListComponent.getElement());
+    render(new TripCreateView
+    (
+      {offerData: this.eventOffers},
+      {destinationData: this.eventDestinations},
+    )
+    , this.eventListComponent.getElement());
 
-    for (let i = 0; i < 3; i++) {
-      render(new TripPointView(), this.eventListComponent.getElement());
+    for (let i = 2; i < 9; i++) {
+      render(new TripPointView
+      (
+        {offerData: this.eventOffers[i]},
+        {destinationData: this.eventDestinations[i]},
+      )
+      , this.eventListComponent.getElement());
     }
   }
 }
