5ce36204ab9b414e4ac1b40ae19230af84bc1064
Eyeballeat <eyeballeat@gmail.com>
2024-05-29 20:06:06 +0200

    добавлено работа с сервером

--- src/points-api-service.js
+++ src/points-api-service.js
@@ -25,7 +25,7 @@ export default class PointsApiService extends ApiService {
     const response = await this._load({
       url: `points/${point.id}`,
       method: Method.PUT,
-      body: JSON.stringify(point),
+      body: JSON.stringify(this.#adaptToServer(point)),
       headers: new Headers({'Content-Type': 'application/json'}),
     });
 
@@ -58,5 +58,21 @@ export default class PointsApiService extends ApiService {
     const parsedResponse = await ApiService.parseResponse(response);
 
     return parsedResponse;
+  }
+
+  #adaptToServer(point) {
+    const adaptedTask = {...point,
+      'base_price': point.basePrice,
+      'date_from': point.dateFrom instanceof Date ? point.dateFrom.toISOString() : null,
+      'date_to': point.dateTo instanceof Date ? point.dateTo.toISOString() : null,
+      'is_favorite': point.isFavorite,
+    };
+
+    delete adaptedTask.basePrice;
+    delete adaptedTask.dateFrom;
+    delete adaptedTask.dateTo;
+    delete adaptedTask.isFavorite;
+
+    return adaptedTask;
   }
 }
